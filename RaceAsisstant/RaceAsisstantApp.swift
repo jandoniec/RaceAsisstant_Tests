22e3adb869c543b27753d2e0ddcb46a0b522ddd9
jandoniec <john@techmaxi.net>
2024-10-17 22:54:38 +0200

    Initial Commit

---
+++ RaceAsisstant/RaceAsisstantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaceAsisstantApp.swift
+//  RaceAsisstant
+//
+//  Created by Jan Doniec on 17/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct RaceAsisstantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RaceAsisstantTests/RaceAsisstantTests.swift
@@ -0,0 +1,16 @@
+//
+//  RaceAsisstantTests.swift
+//  RaceAsisstantTests
+//
+//  Created by Jan Doniec on 17/10/2024.
+//
+
+import Testing
+
+struct RaceAsisstantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
